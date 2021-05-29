5d1945ec7e4f943c6239a67cf8c5e5078ba02cdc
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-29 16:22:26 +0800

    gitflow-feature-stash: unit_scenario

---
+++ app/src/main/java/com/cartoonhero/source/enrollment_android/scene/visitedUnit/UnitScenario.kt
@@ -0,0 +1,11 @@
+package com.cartoonhero.source.enrollment_android.scene.visitedUnit
+
+import com.cartoonhero.source.actormodel.Actor
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
+class UnitScenario: Actor() {
+
+}
